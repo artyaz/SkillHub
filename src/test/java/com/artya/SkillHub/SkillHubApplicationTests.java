c9d07a98495362e258e5384ac19a0b807b413c49
artyaz <achmilenko58@gmail.com>
2023-06-10 12:29:18 +0300

    initial commit

---
+++ src/test/java/com/artya/SkillHub/SkillHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artya.SkillHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
